6683880c93b5d9ff0ba8a6fd5ad48e43693919a9
Samuel Clay <samuel@ofbrooklyn.com>
2012-12-01 16:41:12 -0800

    Adding prod/dev settings for mysql+redis.

---
+++ donationparty/dev_settings.py
@@ -0,0 +1,16 @@
+DATABASES = {
+    'default': {
+        'NAME': 'donationparty',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'donationparty',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+    }
+}
+
+REDIS = {
+    'username': '',
+    'password': '',
+    'host': 'location',
+    'port': 6379,
+}
---
+++ donationparty/prod_settings.py
@@ -0,0 +1,15 @@
+DATABASES = {
+    'default': {
+        'NAME': 'heroku_13e40e0b0a64db0',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'bcfed72b123f20',
+        'PASSWORD': 'e8abd9b7',
+        'HOST': 'us-cdbr-east-02.cleardb.com',
+    }
+}
+REDIS = {
+    'username': 'redistogo',
+    'password': '9261980e79524bf95d732fb2025b43c2',
+    'host': 'ray.redistogo.com',
+    'port': 9649,
+}
