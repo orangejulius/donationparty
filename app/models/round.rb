cc8e26fcd94648e3a3f7f7c049de1ee591b6a287
Julian Simioni <julian.simioni@gmail.com>
2013-01-24 15:54:54 -0800

    Add has_many comments to Round model

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,5 +1,8 @@
 class Round < ActiveRecord::Base
   attr_accessible :charity, :closed, :expire_time, :failed, :max_amount, :secret_token, :url, :winning_address1, :winning_address2
+
+  has_many :donations
+
   after_initialize do |round|
     self.url = SecureRandom.hex(3)
   end
