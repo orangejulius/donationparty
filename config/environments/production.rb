01ca9a0773d72b1a5228f48ae9f29475aacfd46c
Julian Simioni <julian.simioni@gmail.com>
2013-01-25 19:45:43 -0800

    Use pusher key in display view js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Donationparty::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.pusher_key = '10684b1b1c022fc27bbb'
+  config.pusher_secret = 'd8b223f417ee234ad889'
 
   # Code is not reloaded between requests
   config.cache_classes = true
