d72a232d70580a76f99f97f8332046e121a9991e
Julian Simioni <julian.simioni@gmail.com>
2013-01-31 11:47:10 -0800

    Test Donation generates Gravatar URL
    Using app settings for the url format

--- test/unit/donation_test.rb
+++ test/unit/donation_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class DonationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "donation can return gravatar url" do
+    @d = Donation.new(email: 'test1@example.com')
+
+    assert_equal Rails.application.config.gravatar_url % "aa99b351245441b8ca95d54a52d2998c", @d.gravatar_url
+  end
 end
