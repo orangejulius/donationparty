6866881486d815c73212a49c388975647b37e175
Julian Simioni <julian.simioni@gmail.com>
2013-01-31 11:48:02 -0800

    Generate Gravatar url from Donation email

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,8 @@
 class Donation < ActiveRecord::Base
   belongs_to :round
   attr_accessible :amount, :email, :name, :stripe_token
+
+  def gravatar_url
+    Rails.application.config.gravatar_url % Digest::MD5.hexdigest(self.email)
+  end
 end
