864f49d3ee92538b03b256116c1293d1043cfe40
Julian Simioni <julian.simioni@gmail.com>
2013-02-01 00:38:52 -0800

    Test payment form not displayed after payment made

---
+++ test/integration/charge_flow_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class ChargeFlowTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  setup do
+    @round = rounds(:one)
+  end
+
+  test "charge and status don't show payment form after payment" do
+    get '/round_status/' + @round.url
+    @response_json = JSON.parse(@response.body)
+    assert_match '<form', @response_json['payment_info_template']
+
+    post '/charge', stripeToken: 'token', round_id: @round.url, name: 'Test User', email: 'test.email@example.com'
+    @response_json = JSON.parse(@response.body)
+    assert_no_match /<form/, @response_json['payment_info_template']
+
+    get '/round_status/' + @round.url
+    @response_json = JSON.parse(@response.body)
+    assert_no_match /<form/, @response_json['payment_info_template']
+  end
+end
