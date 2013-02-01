07181cdc3fd2bd3deb2add1583aea10d98ca571e
Julian Simioni <julian.simioni@gmail.com>
2013-01-31 19:58:58 -0800

    Add :round to attr_accessible in Donation

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,6 +1,6 @@
 class Donation < ActiveRecord::Base
   belongs_to :round
-  attr_accessible :amount, :email, :name, :stripe_token
+  attr_accessible :amount, :email, :name, :stripe_token, :round
 
   def gravatar_url
     Rails.application.config.gravatar_url % Digest::MD5.hexdigest(self.email)
