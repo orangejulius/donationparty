c8c8024943f86c8dd3d1bb64d81b3d72a193b9d8
Julian Simioni <julian.simioni@gmail.com>
2013-02-04 13:33:46 -0800

    Remove extra self calls

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -3,13 +3,13 @@ class Donation < ActiveRecord::Base
   attr_accessible :amount, :email, :name, :stripe_token, :round
 
   after_initialize do |donation|
-    if self.amount.nil?
+    if amount.nil?
       self.amount = rand(0.0..Rails.application.config.max_donation)
     end
-    self.save
+    save
   end
 
   def gravatar_url
-    Rails.application.config.gravatar_url % Digest::MD5.hexdigest(self.email)
+    Rails.application.config.gravatar_url % Digest::MD5.hexdigest(email)
   end
 end
