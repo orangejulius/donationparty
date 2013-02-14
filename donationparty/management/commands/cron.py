b27eb726d1b59968a9593498cb86af8bdf1d00a2
Julian Simioni <julian.simioni@gmail.com>
2013-02-13 18:03:51 -0800

    Remove python donationparty

--- donationparty/management/commands/cron.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from donationparty.models import *
-
-class Command(BaseCommand):
-    args = 'none'
-    help = 'Runs cron tasks'
-
-    def handle(self, *args, **options):
-        Round.expire_rounds()
