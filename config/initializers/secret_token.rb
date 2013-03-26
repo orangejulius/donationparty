0dea52026b2be0bd83312218f7f706657da36204
Julian Simioni <julian.simioni@gmail.com>
2013-03-25 20:31:41 -0700

    Get rid of Rails 4 related warnings

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Donationparty::Application.config.secret_token = 'a387d096b2919868f239f865596fcf0559ea093f9aea9992a41f266c8a86409457d901b77bef84a7a72717eb24dfa641a8d24ad940542094585de0fcf3ae95af'
+Donationparty::Application.config.secret_key_base = 'lkjhsdahkjsdghiuweiuyewrhjksdjksdfhjkdfshjfdshjkdf'
