6e339db18fcbedabc3e8770c1dc273b8967ed3e1
Julian Simioni <julian.simioni@gmail.com>
2013-04-10 22:32:45 -0700

    Create SecureRandom.hex stubbing helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,4 +15,12 @@ class ActiveSupport::TestCase
   def mock_donation_get_random_amount
     Donation.expects(:get_random_amount).returns(DONATION_AMOUNT)
   end
+
+  def stub_secure_random_hex(size_bytes)
+    SecureRandom.expects(:hex).with(size_bytes).returns(fake_random_hex(size_bytes * 2))
+  end
+
+  def fake_random_hex(size)
+    "x"*size
+  end
 end
