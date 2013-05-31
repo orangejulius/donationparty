6eb47557b232466bd2e72949afdcd5e80c0b47a9
Julian Simioni <julian.simioni@gmail.com>
2013-05-30 23:09:48 -0700

    Simplify Donation model tests

--- test/unit/donation_test.rb
+++ test/unit/donation_test.rb
@@ -1,10 +1,14 @@
 require 'test_helper'
 
 class DonationTest < ActiveSupport::TestCase
+  def setup
+    @donation = Donation.create(email: 'test1@example.com')
+  end
+
   test "donation amount randomly generated on create" do
     mock_donation_get_random_amount
 
-    d = Donation.create(email: 'test1@example.com')
+    d = Donation.new(email: 'test1@example.com')
     assert_equal DONATION_AMOUNT, d.amount
   end
 
@@ -17,39 +21,30 @@ class DonationTest < ActiveSupport::TestCase
   end
 
   test "donation can return gravatar url" do
-    @d = Donation.create(email: 'test1@example.com')
-
-    assert_equal Rails.application.config.gravatar_url % "aa99b351245441b8ca95d54a52d2998c", @d.gravatar_url
+    assert_equal Rails.application.config.gravatar_url % "aa99b351245441b8ca95d54a52d2998c", @donation.gravatar_url
   end
 
   test "donation has randomly generated secret" do
     stub_secure_random_hex(Donation::SECRET_LENGTH_BYTES)
-    @donation = Donation.create(email: 'test1@example.com')
+    donation = Donation.create(email: 'test1@example.com')
 
-    assert_equal fake_random_hex(Donation.secret_length), @donation.secret
+    assert_equal fake_random_hex(Donation.secret_length), donation.secret
   end
 
   test "donation secret persists across save" do
-    @donation = Donation.create(email: 'test@example.com')
+    @donation.save
     @donation2 = Donation.find(@donation.id)
 
     assert_equal @donation.secret, @donation2.secret
   end
 
   test "donation validates presence of email" do
-    @donation = Donation.create
+    @donation.email = nil
 
     assert !@donation.save
-
-    @donation.email = 'test@example.com'
-
-
-    assert @donation.save
   end
 
   test "donation.token returns public token" do
-    @donation = Donation.create(email: 'test@example.com')
-
     assert_match /^[[:xdigit:]]{40}$/, @donation.token
     assert_not_equal @donation.secret, @donation.token
   end
@@ -57,13 +52,13 @@ class DonationTest < ActiveSupport::TestCase
   test "donation can process charge via stripe" do
     token = 'test_stripe_token'
 
-    @donation = Donation.create(stripe_token: token, email: 'test.email@example.com')
+    donation = Donation.create(stripe_token: token, email: 'test.email@example.com')
 
     stripeMock = mock('Charge')
-    stripeMock.expects(:create).with(amount: @donation.cents, currency: 'usd', card: token, description: 'test.email@example.com')
+    stripeMock.expects(:create).with(amount: donation.cents, currency: 'usd', card: token, description: 'test.email@example.com')
 
-    Donation.any_instance.stubs(:chargeObject).returns(stripeMock)
+    donation.stubs(:chargeObject).returns(stripeMock)
 
-    charge = @donation.charge
+    donation.charge
   end
 end
