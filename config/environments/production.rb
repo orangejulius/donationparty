6267d6548fb9ec847ef5c3ef1ae334b2f43b9554
Julian Simioni <julian.simioni@gmail.com>
2013-08-23 23:48:30 -0700

    Fix indentation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Donationparty::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
-	config.eager_load = true
+  config.eager_load = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
