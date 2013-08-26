4a8b188b97133df00b4dea235d63436c21aae156
Julian Simioni <julian.simioni@gmail.com>
2013-08-26 14:44:34 -0700

    Put unicorn pid in correct place

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,3 +5,9 @@ preload_app true
 # Log everything to one file
 stderr_path "log/unicorn.log"
 stdout_path "log/unicorn.log"
+
+app_path = "/home/donationparty/current"
+
+working_directory app_path
+
+pid "#{app_path}/tmp/pids/unicorn.pid"
