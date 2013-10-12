0187a0a4eb420b2fbd0dcce06242a5498212d9fe
Julian Simioni <julian.simioni@gmail.com>
2013-10-12 13:11:56 -0700

    Select charities to display in controller instead of partial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @round = Round.create
+    @charities = Charity.limit(5)
   end
 end
--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -2,9 +2,7 @@ require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
   def setup
-    @charities = []
-    @charities << Charity.create(name: 'Test Charity 1', image_name: 'test1.png')
-    @charities << Charity.create(name: 'Test Charity 2', image_name: 'test2.png')
+    @charities = Charity.limit(5)
 
     get :index
   end
@@ -13,10 +11,19 @@ class HomeControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "index selects 5 charities" do
+    assert assigns(:charities).map(&:class).all? {|c| c == Charity}
+    assert_equal 5, assigns(:charities).count
+  end
+
+  test "index renders charities partial" do
+    assert_template 'home/_charities'
+  end
+
   test "index displays charities" do
     @charities.each do |charity|
-      assert_match 'value="%s"' % charity.id, @response.body
-      assert_match charity.name, @response.body
+      assert_match "value=\"#{charity.id}\"", @response.body
+      assert_match CGI::escapeHTML(charity.name), @response.body
       assert_match charity.image_name, @response.body
     end
   end
