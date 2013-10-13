4023653b84105b4e04d81f53555c7f6dcc5fd686
Julian Simioni <julian.simioni@gmail.com>
2013-10-13 13:51:02 -0700

    Force content type to json
    Required because the partials change the content type to html. Once
    those are removed this won't be a problem.

--- app/controllers/api/rounds_controller.rb
+++ app/controllers/api/rounds_controller.rb
@@ -8,5 +8,6 @@ class Api::RoundsController < ApplicationController
     @donated = cookies['donated_'+@round.url]
     @donations = render_to_string(partial: 'donations', formats: :html)
     @payment_info = render_to_string(partial: 'payment_info', formats: :html)
+    render content_type: 'application/json'  # this is required due to the html format partials
   end
 end
