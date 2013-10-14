829fe598948dd62818db4f3487d61b71f0697cfa
Julian Simioni <julian.simioni@gmail.com>
2013-10-13 18:06:58 -0700

    Split address tests into managable chunks

--- test/controllers/api/addresses_controller_test.rb
+++ test/controllers/api/addresses_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class Api::AddressesControllerTest < ActionController::TestCase
-  test "updating shipping request requires correct round url and donation token" do
+  setup do
     @charity = Charity.create
     @round = Round.create(charity: @charity)
 
@@ -9,21 +9,30 @@ class Api::AddressesControllerTest < ActionController::TestCase
       Donation.create(round: @round, email: 'test@example.com')
     end
 
-    @round.closed = true
-    @round.save
+    @round.update_attribute(:closed, true)
+  end
 
+  test "create with no params returns 403" do
     post :create
     assert_response 403
+  end
 
+  test "create with only valid url returns 403" do
     post :create, url: @round.url
     assert_response 403
+  end
 
+  test "create with only valid token returns 403" do
     post :create, token: @round.winner.token
     assert_response 403
+  end
 
+  test "create with invalid token returns 403" do
     post :create, url: @round.url, token: 'invalid token'
     assert_response 403
+  end
 
+  test "updating shipping request requires correct round url and donation token" do
     post :create, url: @round.url, token: @round.winner.token, address1: '123 a street', address2: 'Apt 23',
       zip_code: '94105', city: 'San Francisco', state: 'CA', country: 'USA'
     assert_redirected_to round_path(@round)
