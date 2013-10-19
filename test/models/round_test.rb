51acac43019c9d26520e620edfc043d0671e5322
Julian Simioni <julian.simioni@gmail.com>
2013-10-18 22:33:54 -0700

    Use more descriptive instance variable name

--- test/models/round_test.rb
+++ test/models/round_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class RoundTest < ActiveSupport::TestCase
   def setup
-    @r = Round.new
+    @round = Round.new
   end
 
   test "newly created round has randomly generated url string" do
@@ -13,21 +13,21 @@ class RoundTest < ActiveSupport::TestCase
   end
 
   test "newly created round has expire time of one hour" do
-    assert_not_nil @r.expire_time
-    assert Time.now + 1.hours - @r.expire_time < 1 # fuzzy time comparison
+    assert_not_nil @round.expire_time
+    assert Time.now + 1.hours - @round.expire_time < 1 # fuzzy time comparison
   end
 
   test "url and expire_time persist on save" do
-    @r.save
-    r2 = Round.find_by url: @r.url
+    @round.save
+    r2 = Round.find_by url: @round.url
 
-    assert_equal r2.url, @r.url
+    assert_equal r2.url, @round.url
 
-    assert_equal r2.expire_time, @r.expire_time
+    assert_equal r2.expire_time, @round.expire_time
   end
 
   test "expire_time does not have miliseconds" do
-    assert @r.expire_time.change(usec: 0) == @r.expire_time, "round.expire_time has milisecond precision"
+    assert @round.expire_time.change(usec: 0) == @round.expire_time, "round.expire_time has milisecond precision"
   end
 
   test "seconds_left returns time until round expires" do
@@ -40,37 +40,37 @@ class RoundTest < ActiveSupport::TestCase
   end
 
   test "seconds_left is never negative" do
-    @r.expire_time = Time.now - 2.hours
-    assert_equal 0, @r.seconds_left
+    @round.expire_time = Time.now - 2.hours
+    assert_equal 0, @round.seconds_left
   end
 
   test "seconds_left has second precision" do
-    assert_equal @r.seconds_left.round, @r.seconds_left
+    assert_equal @round.seconds_left.round, @round.seconds_left
   end
 
   test "expired rounds automatically marked closed" do
-    @r.expire_time = Time.now - Rails.application.config.round_duration - 1.hours
+    @round.expire_time = Time.now - Rails.application.config.round_duration - 1.hours
 
-    assert_equal true, @r.closed
+    assert_equal true, @round.closed
   end
 
   test "winner returns nil if no donations" do
-    assert_nil @r.winner
+    assert_nil @round.winner
   end
 
   test "winner returns donation with highest amount if round finished" do
-    @r.save
+    @round.save
     donations = []
     Rails.application.config.min_donations.times do |i|
-      donations.append Donation.create(round: @r, amount: i, email: 'test@example.com')
+      donations.append Donation.create(round: @round, amount: i, email: 'test@example.com')
     end
 
-    assert_nil @r.winner
+    assert_nil @round.winner
 
-    @r.closed = true
-    @r.save
+    @round.closed = true
+    @round.save
 
-    assert_equal donations.last, @r.winner
+    assert_equal donations.last, @round.winner
   end
 
   test "round failed if closed without enough donations" do
@@ -87,37 +87,37 @@ class RoundTest < ActiveSupport::TestCase
   end
 
   test "total_raised returns total donation amount after round closes successfully" do
-    assert_equal 0, @r.total_raised
+    assert_equal 0, @round.total_raised
 
     donations = []
     Rails.application.config.min_donations.times do |i|
-      donations.append Donation.create(round: @r, amount: i, email: 'test@example.com')
+      donations.append Donation.create(round: @round, amount: i, email: 'test@example.com')
     end
 
-    assert_equal 0, @r.total_raised
+    assert_equal 0, @round.total_raised
 
-    @r.closed = true
+    @round.closed = true
 
-    assert_equal donations.inject(0) {|sum, d| sum + d.amount}, @r.total_raised
+    assert_equal donations.inject(0) {|sum, d| sum + d.amount}, @round.total_raised
   end
 
   test "winner returns nil if round failed with not enough donations" do
-    @r.save
+    @round.save
     @donation = Donation.create(round: @round, email: 'test@example.com')
 
-    @r.closed = true
-    @r.save
-    @r.reload
+    @round.closed = true
+    @round.save
+    @round.reload
 
-    assert_equal nil, @r.winner
+    assert_equal nil, @round.winner
   end
 
   test "notify subscribers triggers pusher event" do
     pusherMock = mock('Pusher')
-    pusherMock.expects(:trigger).with(@r.url, 'new:charge', {})
+    pusherMock.expects(:trigger).with(@round.url, 'new:charge', {})
 
     Round.any_instance.stubs(:realtime).returns(pusherMock)
 
-    @r.notify_subscribers
+    @round.notify_subscribers
   end
 end
